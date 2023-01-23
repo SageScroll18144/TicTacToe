cf8347af75a4c02ab835aaa82af746cc6ceb64dc
SageScroll18144 <ffss@cin.ufpe.br>
2023-01-23 08:01:32 -0300

    implementando exit no menu

--- src/menu.c
+++ src/menu.c
@@ -1,14 +1,16 @@
 #include "raylib.h"
 #include "menu.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int operation;
 
 void runMenu(void){
     operation = -1;
-    printf("-> Select one of the option:\n\t1) PvP.\n\t2) PvM.\n.: ");
-    while(operation!=1 && operation!=2) scanf("%d", &operation);
+    printf("-> Select one of the option:\n\t1) PvP.\n\t2) PvM.\n\t3) Exit.\n.: ");
+    while(operation!=1 && operation!=2 && operation!=3) scanf("%d", &operation);
 }
 int IsPvPorPvM(void){
-    return (operation == 1) ? 0 : 1;//0 para jogar contra outra pessoa, 1 para jogar contra à máquina;
+    if(operation == 3) exit(1);
+    return (operation == 1) ? 0 : (operation == 2) ? 1 : -1;//0 para jogar contra outra pessoa, 1 para jogar contra à máquina;
 }
