4ca49e4c27c181a3f4b26164b47ed50713b3d09a
SageScroll18144 <ffss@cin.ufpe.br>
2023-01-26 18:03:20 -0300

    excluindo o java/objetivo alcançado com C

--- TicTacToe/src/Rules/IllegalMoveException.java
+++
@@ -1,8 +0,0 @@
-package Rules;
-
-@SuppressWarnings("serial")
-public class IllegalMoveException extends Exception{
-    public IllegalMoveException(String message) {
-    	super(message);
-    }
-}
